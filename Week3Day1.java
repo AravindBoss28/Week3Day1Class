7d6409bef4ff62cb993679e9fcd158d1bbbf8bfc
AravindBoss28 <100225085+AravindBoss28@users.noreply.github.com>
2022-03-05 12:46:57 +0530

    Update Week3Day1.java

--- Week3Day1.java
+++ Week3Day1.java
@@ -8,6 +8,24 @@ public class Week3Day1 {
 		String st = "Testleaf is located in Teynampet and above taccobell";
 		String data = "Education";
 		
+		/**
+		 * (?i) ignore case
+		 * Negative lookbehind: (?<!Y)X, matches X, but only if there’s no Y before it.
+		 * if string starts with "T or t" but it should not contain character before it
+		 * incase of the above string "located" also will match. if we don't use this condition.
+		 * 
+		 * t[a-z]+: it will check letter start with "t or T"any thing comes after that
+		 * "+" is used for more than one character matches condition [a-z]
+		 *  
+		 *  X(?=Y)	Positive lookahead	X if followed by Y 
+                 *  X(?!Y)	Negative lookahead	X if not followed by Y
+                 *  (?<=Y)X	Positive lookbehind	X if after Y
+                 *  (?<!Y)X	Negative lookbehind	X if not after Y  
+                 *  (?!\\S) Negative lookbehind
+                 *  matches string without white space
+                 *  
+		 * */
+
 		Pattern pattern = Pattern.compile("(?i)(?<!\\\\S)t[a-z]+(?!\\\\S)");
 		String[] result = st.split(" ");
 		for (int i = 0; i < result.length; i++) {
@@ -16,7 +34,11 @@ public class Week3Day1 {
 				System.out.println(result[i]);
 			}
 		}
-
+                
+		/**
+		* (?i) ignore case
+		* [aeiou] character matches a,e,i,o,u
+		*/
 		Pattern pat1 = Pattern.compile("(?i)[aeiou]");
 		char[] ed = data.toCharArray();
 		int count = 0;
